c46031921f018848751614a73a3a30b85bdcebe7
Avazbek <qalandarovavazbek43@gmail.com>
2025-01-15 10:45:22 +0500

    port o'zgardi

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ const bodyParser = require('body-parser')
 const cookieParser = require('cookie-parser')
 const cors = require('cors')
 const path = require('path')
+require('dotenv').config()
 
 connectDB()
 const app = express()
@@ -24,6 +25,7 @@ app.use(cookieParser(process.env.COOKIE_PARSER_KEY))
 
 // routerni o'rnatish
 appRouter(app)
+const PORT = process.env.PORT || 3000
 
 app.listen(3000, () => {
     console.log(`Server is running on Port: 3000... `);
