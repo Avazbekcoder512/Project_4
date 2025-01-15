51ea863a7ee5426d6cd5d63bb5f50c1e0a0ddf45
Avazbek <qalandarovavazbek43@gmail.com>
2025-01-15 10:04:02 +0500

    routes o'zgardi

--- src/Router/router.js
+++ src/Router/router.js
@@ -12,18 +12,18 @@ const analysisResultRouter = require('./analysisResultRouter')
 const resultRouter = require('./resultRouter')
 
 exports.appRouter = (app) => {
-    app.use('/api', resultRouter)
+    app.use('/', resultRouter)
     
-    app.use('/api', loginRouter)
-    app.use('/api', jwtAccessMiddleware, adminRouter)
-    app.use('/api', jwtAccessMiddleware, doctorRouter)
-    app.use('/api', jwtAccessMiddleware, staffRouter)
-    app.use('/api', jwtAccessMiddleware, serviceRouter)
-    app.use('/api', jwtAccessMiddleware, newsRouter)
-    app.use('/api', jwtAccessMiddleware, patientRouter)
-    app.use('/api', jwtAccessMiddleware, analysisRouter)
-    app.use('/api', jwtAccessMiddleware, sectionRouter)
-    app.use('/api', jwtAccessMiddleware, analysisResultRouter)
+    app.use('/', loginRouter)
+    app.use('/', jwtAccessMiddleware, adminRouter)
+    app.use('/', jwtAccessMiddleware, doctorRouter)
+    app.use('/', jwtAccessMiddleware, staffRouter)
+    app.use('/', jwtAccessMiddleware, serviceRouter)
+    app.use('/', jwtAccessMiddleware, newsRouter)
+    app.use('/', jwtAccessMiddleware, patientRouter)
+    app.use('/', jwtAccessMiddleware, analysisRouter)
+    app.use('/', jwtAccessMiddleware, sectionRouter)
+    app.use('/', jwtAccessMiddleware, analysisResultRouter)
 
     app.use((req, res) => {
         return res.status(404).send({
