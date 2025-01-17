6e164c9ccb2557f2f7cfdad75449e3bb56400cb2
Avazbek <qalandarovavazbek43@gmail.com>
2025-01-17 16:13:56 +0500

    Xodimlar uchun parolni yangilash tayyor

--- src/validators/staffValidate.js
+++ src/validators/staffValidate.js
@@ -43,8 +43,6 @@ exports.updateStaffSchema = {
 
     username: { isString: { errorMessage: "Foydalanuvchi nomi string bo'lishi kerak!" }, trim: { errorMessage: "Foydalanuvchi nomining orasida ochiq joy bo'lishi mumkin emas!" } },
 
-    password: { isString: { errorMessage: "Parol string bo'lishi kerak!" }, trim: { errorMessage: "Parol orasida ochiq joy bo'lishi mumkin emas!" } },
-
     uz_description: { isString: { errorMessage: "Tavsif string bo'lishi kerak!" }, isLength: { options: { min: 25 }, errorMessage: "Tavsif kamida 25 ta belgidan iborat bo'lishi kerak!" }, },
 
     ru_description: { isString: { errorMessage: "Tavsif string bo'lishi kerak!" }, isLength: { options: { min: 25 }, errorMessage: "Tavsif kamida 25 ta belgidan iborat bo'lishi kerak!" }, },
