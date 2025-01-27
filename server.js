39e4903d0e9580ff8e487e2f311aa358fc531a74
Avazbekcoder512 <qalandarovavazbek43@gmail.com>
2025-01-27 12:26:12 +0500

    server.js da o'zgarish

--- server.js
+++ server.js
@@ -25,6 +25,6 @@ app.use(cookieParser(process.env.COOKIE_PARSER_KEY))
 appRouter(app)
 const PORT = process.env.PORT || 3000
 
-app.listen(3000, () => {
+app.listen(PORT, () => {
     console.log(`Server is running on Port: ${PORT}... `);
 })
