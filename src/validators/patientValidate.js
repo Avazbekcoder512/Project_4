858b2108384a2496b6ae0ac31e42a2f8c8322a02
Avazbekcoder512 <qalandarovavazbek43@gmail.com>
2025-01-27 12:07:54 +0500

    patientda o'zgarish

--- src/validators/patientValidate.js
+++ src/validators/patientValidate.js
@@ -1,17 +1,69 @@
 exports.createPatientSchema = {
-    name: { isString: { errorMessage: "F.I.Sh  string bo'lishi kerak!" }, notEmpty: { errorMessage: "F.I.Sh talab qilinadi!" } },
-    date_of_birth: { isString: { errorMessage: "Tu'g'ilgan kun string bo'lishi kerak!" }, notEmpty: { errorMessage: "Tu'g'ilgan kun talab qilinadi!" } },
-    gender: { isString: { errorMessage: "Gender string bo'lishi kerak!" }, notEmpty: { errorMessage: "Gender talab qilinadi!" } },
-    
-    email: { isEmail: { errorMessage: "Elektron pochta manzili yaroqsiz!" }, notEmpty: { errorMessage: "Elektron pochta manzili talab qilinadi!" } },
+  name: {
+    isString: { errorMessage: "F.I.Sh  string bo'lishi kerak!" },
+    notEmpty: { errorMessage: "F.I.Sh talab qilinadi!" },
+  },
+  date_of_birth: {
+    isString: { errorMessage: "Tu'g'ilgan kun string bo'lishi kerak!" },
+    notEmpty: { errorMessage: "Tu'g'ilgan kun talab qilinadi!" },
+    custom: {
+      options: (value) => {
+        const regex = /^\d{2}-\d{2}-\d{4}$/;
+        if (!regex.test(value)) {
+          throw new Error(
+            "Tu'g'ilgan kun noto'g'ri formatda. DD-MM-YYYY formatida bo'lishi kerak!"
+          );
+        }
 
-    orderNumber: { isString: { errorMessage: "Tartib raqami raqam bo'lishi kerak!" }, notEmpty: { errorMessage: "Tartib raqami talab qilinadi!" } },
-}
+        const [day, month, year] = value.split("-").map(Number);
+        const isValidDate = !isNaN(new Date(year, month - 1, day).getTime());
+        if (!isValidDate || day < 1 || day > 31 || month < 1 || month > 12) {
+          throw new Error("Tu'g'ilgan kun haqiqiy sana emas!");
+        }
+        return true;
+      },
+    },
+  },
+  gender: {
+    isString: { errorMessage: "Gender string bo'lishi kerak!" },
+    notEmpty: { errorMessage: "Gender talab qilinadi!" },
+  },
+
+  email: {
+    isEmail: { errorMessage: "Elektron pochta manzili yaroqsiz!" },
+    notEmpty: { errorMessage: "Elektron pochta manzili talab qilinadi!" },
+  },
+
+  orderNumber: {
+    isString: { errorMessage: "Tartib raqami raqam bo'lishi kerak!" },
+    notEmpty: { errorMessage: "Tartib raqami talab qilinadi!" },
+  },
+};
 
 exports.updatePateintSchema = {
-    name: { isString: { errorMessage: "F.I.Sh  string bo'lishi kerak!" } },
-    date_of_birth: { isString: { errorMessage: "Tu'g'ilgan kun string bo'lishi kerak!" } },
-    gender: { isString: { errorMessage: "Gender string bo'lishi kerak!" } },
-    
-    email: { isEmail: { errorMessage: "Elektron pochta manzili yaroqsiz!" } },
-}
+  name: { isString: { errorMessage: "F.I.Sh  string bo'lishi kerak!" } },
+  date_of_birth: {
+    isString: { errorMessage: "Tu'g'ilgan kun string bo'lishi kerak!" },
+    notEmpty: { errorMessage: "Tu'g'ilgan kun talab qilinadi!" },
+    custom: {
+      options: (value) => {
+        const regex = /^\d{2}-\d{2}-\d{4}$/;
+        if (!regex.test(value)) {
+          throw new Error(
+            "Tu'g'ilgan kun noto'g'ri formatda. DD-MM-YYYY formatida bo'lishi kerak!"
+          );
+        }
+
+        const [day, month, year] = value.split("-").map(Number);
+        const isValidDate = !isNaN(new Date(year, month - 1, day).getTime());
+        if (!isValidDate || day < 1 || day > 31 || month < 1 || month > 12) {
+          throw new Error("Tu'g'ilgan kun haqiqiy sana emas!");
+        }
+        return true;
+      },
+    },
+  },
+  gender: { isString: { errorMessage: "Gender string bo'lishi kerak!" } },
+
+  email: { isEmail: { errorMessage: "Elektron pochta manzili yaroqsiz!" } },
+};
