f3513d56012f039a699adfea4d4d70240d00149c
Avazbekcoder512 <qalandarovavazbek43@gmail.com>
2025-02-11 15:29:11 +0500

    test

--- server.js
+++ server.js
@@ -1,32 +1,33 @@
-const express = require('express');
-const { connectDB } = require("./src/database/connect");
-const { appRouter } = require('./src/Router/router');
-const bodyParser = require('body-parser');
-const cookieParser = require('cookie-parser');
-const cors = require('cors');
-const path = require('path');
-const { bot } = require('./src/bot/bot');
-require('dotenv').config();
+const express = require('express')
+const { connectDB } = require("./src/database/connect")
+const { appRouter } = require('./src/Router/router')
+const bodyParser = require('body-parser')
+const cookieParser = require('cookie-parser')
+const cors = require('cors')
+const path = require('path')
+const { runBot } = require('./src/bot/bot')
+require('dotenv').config()
 
-connectDB();
+connectDB()
+runBot()
 
-const app = express();
+const app = express()
+
 app.use(cors());
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
+
+// bodyni pars qilish
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true }))
+
 app.use(express.static(path.join(__dirname, 'public')));
-app.use(cookieParser(process.env.COOKIE_PARSER_KEY));
-appRouter(app);
 
-app.use(`/${process.env.BOT_TOKEN}`, express.json(), (req, res) => {
-    bot.handleUpdate(req.body, res);
-});
+// Cookie parserni o'rnatig
+app.use(cookieParser(process.env.COOKIE_PARSER_KEY))
 
-bot.api.setWebhook(`${process.env.WEBHOOK_URL}/${process.env.BOT_TOKEN}`);
-    console.log('✅ Webhook o‘rnatildi:', `${process.env.WEBHOOK_URL}/${process.env.BOT_TOKEN}`);
+// routerni o'rnatish
+appRouter(app)
+const PORT = process.env.PORT || 3000
 
-const PORT = process.env.PORT || 3000;
-
-app.listen(PORT, async () => {
+app.listen(PORT, () => {
     console.log(`Server is running on Port: ${PORT}... `);
-});
+})
