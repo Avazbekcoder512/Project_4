5ec6083da1a2144fbc7c80de94bbb4105968e9bb
Avazbekcoder512 <qalandarovavazbek43@gmail.com>
2025-02-20 17:53:32 +0500

    test

--- src/bot/registration.js
+++ src/bot/registration.js
@@ -2,7 +2,7 @@ const { InlineKeyboard } = require("grammy");
 
 exports.registration = async (ctx) => {
     try {
-        const keyboard = new InlineKeyboard().webApp("Formani to'ldirish", new WebAppInfo())
+        const keyboard = new InlineKeyboard().webApp("Formani to'ldirish", "https://webapp-psi-tawny.vercel.app/")
 
         await ctx.reply("Qabulga yozilish uchun quyidagi formani to'ldiring:", {
             reply_markup: keyboard
