5aef87a5bee9c19ed42e1b86c31d4614f672dbed
Avazbekcoder512 <qalandarovavazbek43@gmail.com>
2025-02-21 17:44:58 +0500

    test

--- src/bot/registration.js
+++ src/bot/registration.js
@@ -1,8 +1,10 @@
 const { Keyboard } = require("grammy");
+const { text } = require("pdfkit");
 
 exports.registration = async (ctx) => {
     try {
-        const keyboard = new Keyboard().webApp("Formani to'ldirish", "https://webapp-psi-tawny.vercel.app/").resized()
+        const keyboard = new Keyboard().webApp("Formani to'ldirish", "https://webapp-psi-tawny.vercel.app/").row()
+        .text("📋 Menyu").resized()
 
         await ctx.reply("Qabulga yozilish uchun quyidagi formani to'ldiring:", {
             reply_markup: keyboard
