bf5b21fbf242b3677c700b4cf90dd968d90d6ecb
Avazbekcoder512 <qalandarovavazbek43@gmail.com>
2025-02-22 17:26:09 +0500

    botga o'zgartirish kiritildi

---
+++ src/bot/profile.js
@@ -0,0 +1,36 @@
+const { userModel } = require("../models/usersModel");
+
+exports.uzProfile = async (ctx) => {
+    try {
+        const user = await userModel.findOne({ chatId: ctx.chat.id })
+
+        if (user.email) {
+            await ctx.reply(`
+                Sizning ma'lumotlaringiz:\n\n
+<b>Ismingiz:</b> ${user.name}\n\n
+<b>Foydalanuvchi ismi:</b> @${user.username}\n\n
+<b>Tug'ilgan kuningiz:</b> ${user.date_of_birth}\n\n
+<b>Viloyatingiz:</b> ${user.region}\n\n
+<b>Shahar yoki tumaningiz:</b> ${user.district}\n\n
+<b>Mahallangiz:</b> ${user.district}\n\n
+<b>Ko'changiz:</b> ${user.street}\n\n
+<b>Uy raqamingiz:</b> ${user.house}\n\n
+<b>Emailingiz:</b> ${user.email}\n\n
+<b>Telefon raqamingiz:</b> ${user.phoneNumber}\n\n
+<b>Tanlagan xizmatingiz:</b> ${user.service}`, {
+                parse_mode: "HTML"
+            })
+        } else {
+            await ctx.reply(`
+            Sizning ma'lumotlaringiz:
+<b>Ismingiz:</b> ${user.name}
+<b>Foydalanuvchi ismi:</b> @${user.username}\n\n
+`, {
+                parse_mode: "HTML"
+            })
+        }
+    } catch (error) {
+        console.log(error);
+        ctx.reply('🆘  Serverda xatolik!');
+    }
+}
--- src/bot/registration.js
+++ src/bot/registration.js
@@ -1,9 +1,8 @@
 const { Keyboard } = require("grammy");
-const { text } = require("pdfkit");
 
 exports.registration = async (ctx) => {
     try {
-        const keyboard = new Keyboard().webApp("Formani to'ldirish", "https://webapp-psi-tawny.vercel.app/").row()
+        const keyboard = new Keyboard().webApp("✍️  Formani to'ldirish", "https://webapp-psi-tawny.vercel.app/").row()
         .text("📋 Menyu").resized()
 
         await ctx.reply("Qabulga yozilish uchun quyidagi formani to'ldiring:", {
