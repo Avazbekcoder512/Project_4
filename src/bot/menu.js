4dd15841c6655194c742f040c5f3f2fc9e59bbfc
Avazbekcoder512 <qalandarovavazbek43@gmail.com>
2025-02-28 16:32:05 +0500

    test

--- src/bot/menu.js
+++ src/bot/menu.js
@@ -13,74 +13,78 @@ const { ruResult } = require("./result/ruResult");
 const { enResult } = require("./result/enResult");
 const { registration } = require("./registration");
 const { uzProfile, ruProfile, enProfile } = require("./profile");
+const { patientModel } = require("../models/patientModel");
 
 exports.Menyu = async (text, ctx) => {
-    switch (text) {
-        case "📋 Menyu":
-            await uzMenyu(ctx);
-            break;
-        case "📋 Меню":
-            await ruMenyu(ctx);
-            break;
-        case "📋 Menu":
-            await enMenyu(ctx);
-            break;
-        case "🧑‍⚕️  Shifokorlar":
-            await uzDoctors(ctx);
-            break;
-        case "🧑‍⚕️  Врачи":
-            await ruDoctors(ctx);
-            break;
-        case "🧑‍⚕️  Doctors":
-            await enDoctors(ctx);
-            break;
-        case "🩺  Xizmatlar":
-            await uzService(ctx);
-            break;
-        case "🩺  Услуги":
-            await ruService(ctx);
-            break;
-        case "🩺  Services":
-            await enService(ctx);
-            break;
-        case "💵  Tahlil narxlari":
-            await uzSections(ctx);
-            break;
-        case "💵  Анализ цен":
-            await ruSections(ctx);
-            break;
-        case "💵  Analysis prices":
-            await enSections(ctx);
-            break;
-        case "🧬  Tahlil natijasi":
-            await uzResult(ctx);
-            break;
-        case "🧬  Результат анализа":
-            await ruResult(ctx);
-            break;
-        case "🧬  Analysis result":
-            await enResult(ctx);
-            break;
-        case "📝  Qabulga yozilish":
-            await registration(ctx);
-            break;
-        case "📝  Регистрация":
-            await registration(ctx);
-            break;
-        case "📝  Registration":
-            await registration(ctx);
-            break;
-        case "👤  Ma'lumotlarim":
-            await uzProfile(ctx);
-            break;
-        case "👤  Моя информация":
-            await ruProfile(ctx);
-            break;
-        case "👤  My information":
-            await enProfile(ctx);
-            break;
-        // default:
-        //     await ctx.reply("📌 Iltimos, menyudagi tugmalardan foydalaning.");
-        //     break;
+    const user = await patientModel.findOne({chatId: ctx.from.id})
+    if (user.action === "Start") {
+        switch (text) {
+            case "📋 Menyu":
+                await uzMenyu(ctx);
+                break;
+            case "📋 Меню":
+                await ruMenyu(ctx);
+                break;
+            case "📋 Menu":
+                await enMenyu(ctx);
+                break;
+            case "🧑‍⚕️  Shifokorlar":
+                await uzDoctors(ctx);
+                break;
+            case "🧑‍⚕️  Врачи":
+                await ruDoctors(ctx);
+                break;
+            case "🧑‍⚕️  Doctors":
+                await enDoctors(ctx);
+                break;
+            case "🩺  Xizmatlar":
+                await uzService(ctx);
+                break;
+            case "🩺  Услуги":
+                await ruService(ctx);
+                break;
+            case "🩺  Services":
+                await enService(ctx);
+                break;
+            case "💵  Tahlil narxlari":
+                await uzSections(ctx);
+                break;
+            case "💵  Анализ цен":
+                await ruSections(ctx);
+                break;
+            case "💵  Analysis prices":
+                await enSections(ctx);
+                break;
+            case "🧬  Tahlil natijasi":
+                await uzResult(ctx);
+                break;
+            case "🧬  Результат анализа":
+                await ruResult(ctx);
+                break;
+            case "🧬  Analysis result":
+                await enResult(ctx);
+                break;
+            case "📝  Qabulga yozilish":
+                await registration(ctx);
+                break;
+            case "📝  Регистрация":
+                await registration(ctx);
+                break;
+            case "📝  Registration":
+                await registration(ctx);
+                break;
+            case "👤  Ma'lumotlarim":
+                await uzProfile(ctx);
+                break;
+            case "👤  Моя информация":
+                await ruProfile(ctx);
+                break;
+            case "👤  My information":
+                await enProfile(ctx);
+                break;
+            // default:
+            //     await ctx.reply("📌 Iltimos, menyudagi tugmalardan foydalaning.");
+            //     break;
+        }
     }
 }
--- src/bot/registration.js
+++ src/bot/registration.js
@@ -1,6 +1,5 @@
 const { Keyboard } = require("grammy");
 const { patientModel } = require("../models/patientModel");
-const { uzMenyu } = require("./menyuKeyboard");
 
 const uzContactKeyboard = new Keyboard()
     .requestContact("📞 Telefon raqamni yuborish")
@@ -31,13 +30,13 @@ exports.askNextStep = async (ctx, user) => {
     } else if (user.step === 2) {
         if (user.language === "Language-Uzb") {
             await ctx.reply(`🎂 Tu'gulgan <b>Kun</b>, <b>Oy</b> va <b>Yilingizni</b> kiriting 
-<b>Masalan:</b>  25.05.2025`, { parse_mode: "HTML" });
+<b>Masalan:</b>  25-05-2025`, { parse_mode: "HTML" });
         } else if (user.language === "Language-Rus") {
             await ctx.reply(`🎂 Введите <b>День</b>, <b>Месяц</b> и <b>Год</b> рождения
-<b>Например:</b> 25.05.2025`, { parse_mode: "HTML" });
+<b>Например:</b> 25-05-2025`, { parse_mode: "HTML" });
         } else if (user.language === "Language-Eng") {
             await ctx.reply(`🎂 Enter your <b>Day</b>, <b>Month</b> and <b>Year</b> of birth
-<b>For example:</b> 25.05.2025`, { parse_mode: "HTML" });
+<b>For example:</b> 25-05-2025`, { parse_mode: "HTML" });
         }
     } else if (user.step === 3) {
         if (user.language === "Language-Uzb") {
@@ -76,7 +75,8 @@ exports.registration = async (ctx) => {
 Please restart the bot by pressing /start!`)
         }
 
-        if (!user.step || user.step === 0) {
+        if (!user.step || user.step === 0 || user.step === 7) {
+            user.action = "Registration"
             user.step = 1
             await user.save()
             return this.askNextStep(ctx, user);
